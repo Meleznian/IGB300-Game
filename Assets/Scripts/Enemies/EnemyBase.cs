61057c0e79f55a5c5971cf575775199dc0091f34
Meleznian <jwinmeleznia@gmail.com>
2025-04-01 16:32:48 +1000

    Fixed some incorrect capitalization

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -3,33 +3,33 @@ using System;
 
 public class EnemyBase : MonoBehaviour
 {
-    [ToolTip("The enemies name")]
+    [Tooltip("The enemies name")]
     public string enemyName;
 
     [Header("Enemy Stats")]
-    [ToolTip("How much health the enemy starts with")]
+    [Tooltip("How much health the enemy starts with")]
     public float health;
-    [ToolTip("How fast does the enemy move")]
+    [Tooltip("How fast does the enemy move")]
     public float moveSpeed;
-    [ToolTip("How long between each attack")]
+    [Tooltip("How long between each attack")]
     public float attackSpeed;
-    [ToolTip("How long is it stunned after being parried")]
+    [Tooltip("How long is it stunned after being parried")]
     public float stunTime;
-    [ToolTip("How much resistance does the enemy have to being knocked back by the player")]
+    [Tooltip("How much resistance does the enemy have to being knocked back by the player")]
     public float knockbackResist;
 
     [Header("Attacks")]
-    [ToolTip("List of attacks the enemy can do")]
+    [Tooltip("List of attacks the enemy can do")]
     public Attack[] attacks;
 
     [Serializable]
     public class Attack
     {
-        [ToolTip("Attack identifier")]
+        [Tooltip("Attack identifier")]
         public string ID;
-        [ToolTip("How much damage will the attack do")]
+        [Tooltip("How much damage will the attack do")]
         public float damage;
-        [ToolTip("how much force will the enemy knock the player back with")]
+        [Tooltip("how much force will the enemy knock the player back with")]
         public float knockback;
     }
 
