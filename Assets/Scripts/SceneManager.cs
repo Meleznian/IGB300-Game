46eca47bc8c91d4779d3d23662eb6a4fe7bec271
RexLegendes <166970739+RexLegendes@users.noreply.github.com>
2025-04-03 20:46:07 +1000

    Rex 03/04/2025 UI Design
    Creating 3 buttons (Start, Setting and End)
    Script created

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
