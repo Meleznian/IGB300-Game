ec4629dbbca54cdfd39c90cdd424304eeafec28b
RexLegendes <166970739+RexLegendes@users.noreply.github.com>
2025-04-11 09:31:19 +1000

    Start Scene - Rex 11/04/2025

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject creditsPanel;
+    public GameObject MainCanvas;
+    public Button closeButton;
+    public Button creditButton;
+
+    void Start()
+    {
+        closeButton.onClick.AddListener(CloseCredits);
+        creditButton.onClick.AddListener(OpenCredits);
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+        MainCanvas.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        MainCanvas.SetActive(true);
+    }
+}
