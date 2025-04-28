2736eb4669993bd77ec75c2dc10132e521096f1a
Meleznian <jwinmeleznia@gmail.com>
2025-04-28 11:53:07 +1000

    Added  updated charger pathfinding

--- Assets/Scripts/Navigation/LinkedNodes.cs
+++ Assets/Scripts/Navigation/LinkedNodes.cs
@@ -5,6 +5,8 @@ public class LinkedNodes : MonoBehaviour
 {
     //Index of this node
     public int index;
+    public bool prebuilt;
+
 
     //Game Objects of each linked node
     public List<GameObject> linkedNodeObjects;
@@ -15,14 +17,17 @@ public class LinkedNodes : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Creates Linked Nodes array
-        //linkedNodesIndex = new List<int>();
-
-        /*//Fills Linked Nodes array
-        for(int i = 0; i < linkedNodeObjects.Count; i++)
+        if (prebuilt)
         {
-            linkedNodesIndex[i] = linkedNodeObjects[i].GetComponent<LinkedNodes>().index;
-        }*/
+            //Creates Linked Nodes array
+            linkedNodesIndex = new List<int>();
+
+            //Fills Linked Nodes array
+            for (int i = 0; i < linkedNodeObjects.Count; i++)
+            {
+                linkedNodesIndex.Add(linkedNodeObjects[i].GetComponent<LinkedNodes>().index);
+            }
+        }
     }
 
     public void AddConnection(GameObject to)
