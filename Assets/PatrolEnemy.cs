39d67887609fffb17ad5667ff3f18e780a768c38
ZodicLight <40487226+ZodicLight@users.noreply.github.com>
2025-05-08 03:08:32 +1000

    PatrolEnemyPt2

--- Assets/PatrolEnemy.cs
+++ Assets/PatrolEnemy.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PatrolEnemy : MonoBehaviour
 {
+
+    public float moveSpeed = 2f;
+    public Transform checkPoint;
+    public float distance = 1f;
+    public LayerMask layerMask;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,8 @@ public class PatrolEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime);
+        transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+
+        //Physics2D.Raycast()
     }
 }
