e58d3ddbd9c1e2db3034195be5ebdbf14390a94e
StellaStar1 <n11540354@qut.edu.au>
2025-05-15 13:21:44 +1000

    Checks for when Bounce

--- Assets/CurrencyObject.cs
+++ Assets/CurrencyObject.cs
@@ -5,6 +5,14 @@ public class CurrencyObject : MonoBehaviour, ICollectable
     public CurrencyHolder storage;
     public int value;
 
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Debug.Log("Bounce");
+        }
+    }
+
     public void Collect()
     {
         //Perform Collection Process -- Number Go Up
