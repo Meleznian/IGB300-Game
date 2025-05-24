25b6274b5d0ba0f688fa03eeaa5a5eadc4d9bb88
Yuma0707 <hillzenn@gmail.com>
2025-05-24 13:27:11 +1000

    Add Cooldown in Melee and Range

--- Assets/Scripts/Player/PlayerRangedAttack.cs
+++ Assets/Scripts/Player/PlayerRangedAttack.cs
@@ -6,20 +6,27 @@ public class PlayerRangedAttack : MonoBehaviour
     [SerializeField] Transform firePoint;
     [SerializeField] Transform aimCursor;
 
+    [SerializeField] float rangedCooldown = 0.3f;
+    float rangedCooldownTimer = 0f;
+
     void Update()
     {
+        rangedCooldownTimer -= Time.deltaTime;
+
         // Right click with mouse
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && rangedCooldownTimer <= 0f)
         {
             Vector2 dir = aimCursor.position - firePoint.position;
             FireBullet(dir.normalized);
+            rangedCooldownTimer = rangedCooldown;
         }
 
         // Controller: press R2
-        if (Input.GetKeyDown(KeyCode.JoystickButton7))
+        if (Input.GetKeyDown(KeyCode.JoystickButton7) && rangedCooldownTimer <= 0f)
         {
             Vector2 dir = aimCursor.position - firePoint.position;
             FireBullet(dir.normalized);
+            rangedCooldownTimer = rangedCooldown;
         }
     }
 
