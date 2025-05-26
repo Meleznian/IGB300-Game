127ef3bef892e25ca7c00813e107f065504f33d4
Meleznian <jwinmeleznia@gmail.com>
2025-05-26 14:55:49 +1000

    Updated Parry

--- Assets/Scripts/ParryRelay.cs
+++ Assets/Scripts/ParryRelay.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class ParryRelay : MonoBehaviour
 {
     [SerializeField] PlayerHealth health;
+    [SerializeField] Parry parry;
 
-    public  void StartParry()
+    public void StartParry()
     {
         health.StartParry();
+        parry.StartParry();
     }
     public void EndParry()
     {
         health.EndParry();
+        parry.EndParry();   
     }
 }
