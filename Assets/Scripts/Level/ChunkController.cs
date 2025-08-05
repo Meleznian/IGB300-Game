3b03e4382be1318716b706b145e23bc2dd530a00
Paul N <deul1st.official@gmail.com>
2025-08-05 15:27:59 +1000

    level chunks added and basic system now works

--- Assets/Scripts/Level/ChunkController.cs
+++ Assets/Scripts/Level/ChunkController.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class ChunkController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int _poolIndex;
+    [SerializeField] private float _despawnX = -30f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (transform.position.x < _despawnX)
+        {
+            ChunkPoolManager.Instance.ReturnChunkToPool(gameObject, _poolIndex);
+        }
     }
 }
--- Assets/Scripts/Level/ChunkSpawner.cs
+++ Assets/Scripts/Level/ChunkSpawner.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.Android;
 
 public class ChunkSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject[] _chunkPrefabs;
+    [SerializeField] private float _chunkWidth = 20f;
+    [SerializeField] private int _chunksAhead = 3;
+
+    private float _spawnX = 0f;
+
     void Start()
     {
-        
+        for (int i = 0; i < _chunksAhead; i++)
+        {
+            SpawnChunk();
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
+        if (Camera.main.transform.position.x + (_chunksAhead * _chunkWidth) > _spawnX)
+        {
+            SpawnChunk();
+        }
+      
+    }
+
+    void SpawnChunk()
+    {
+        int randomIndex = Random.Range(0, ChunkPoolManager.Instance.PoolCount);
+        GameObject chunk = ChunkPoolManager.Instance.GetChunkFromPool(randomIndex, new Vector3(_spawnX, 0f, 0f));
         
+        _spawnX += _chunkWidth;
     }
 }
