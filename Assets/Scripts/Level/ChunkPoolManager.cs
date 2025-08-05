a804c6f69a20eb025119429ff6bab70e53ae7213
Paul <162394579+Duel1st-Official@users.noreply.github.com>
2025-08-05 15:35:41 +1000

    level chunks added and basic system now works (#16)

--- Assets/Scripts/Level/ChunkPoolManager.cs
+++ Assets/Scripts/Level/ChunkPoolManager.cs
@@ -1,16 +1,59 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ChunkPoolManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public class ChunkPool
     {
-        
+        public GameObject chunkPrefab;
+        public int poolSize = 5;
+        public Queue<GameObject> poolQueue = new Queue<GameObject>();
+    }
+    
+    [SerializeField] private ChunkPool[] _chunksPool;
+    
+    public static ChunkPoolManager Instance { get; private set; }
+    
+    void Awake()
+    {
+        if(Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        foreach (var pool in _chunksPool)
+        {
+            for (int i = 0; i < pool.poolSize; i++)
+            {
+                GameObject obj = Instantiate(pool.chunkPrefab);
+                obj.SetActive(false);
+                pool.poolQueue.Enqueue(obj);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetChunkFromPool(int poolIndex, Vector3 position)
     {
+        ChunkPool pool = _chunksPool[poolIndex];
+
+        if (pool.poolQueue.Count == 0)
+        {
+            Debug.LogWarning("Pool exhausted, adding to the pool");
+            GameObject extra = Instantiate(pool.chunkPrefab);
+            extra.SetActive(false);
+            pool.poolQueue.Enqueue(extra);
+        }
         
+        GameObject chunk = pool.poolQueue.Dequeue();
+        chunk.transform.position = position;
+        chunk.SetActive(true);
+        return chunk;
     }
+
+    public void ReturnChunkToPool(GameObject chunk, int poolIndex)
+    {
+        chunk.SetActive(false);
+        _chunksPool[poolIndex].poolQueue.Enqueue(chunk);
+    }
+    
+    public int PoolCount => _chunksPool.Length;
 }
