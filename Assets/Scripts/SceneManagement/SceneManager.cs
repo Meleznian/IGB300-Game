b3d17122ebf5773ec07df43fb042e85e7afcc2ba
RexLegendes <166970739+RexLegendes@users.noreply.github.com>
2025-08-15 21:05:10 +1000

    start scene updated
    scoreboard menu

--- Assets/Scripts/SceneManagement/SceneManager.cs
+++ Assets/Scripts/SceneManagement/SceneManager.cs
@@ -1,36 +1,62 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using System.Collections;
 using TMPro;
+
 public class SettingsManager : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject TutorialPanel;
+    public GameObject ScoreboardPanel;
+
+    private GameObject currentOpenPanel;
 
     public void Start()
     {
-
         TutorialPanel.SetActive(false);
+        ScoreboardPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
+
     public void Tutorial()
     {
+        CloseCurrentPanel();
         TutorialPanel.SetActive(true);
+        currentOpenPanel = TutorialPanel;
     }
+
+    public void Scoreboard()
+    {
+        CloseCurrentPanel();
+        ScoreboardPanel.SetActive(true);
+        currentOpenPanel = ScoreboardPanel;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("MVPLevel");
     }
+
     public void OpenSettings()
     {
         PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
         SceneManager.LoadScene("Setting Scene");
     }
+
     public void GoBackToMainMenu()
     {
-
         mainMenuPanel.SetActive(true);
     }
+
+    public void CloseCurrentPanel()
+    {
+        if (currentOpenPanel != null)
+        {
+            currentOpenPanel.SetActive(false);
+            currentOpenPanel = null;
+        }
+        mainMenuPanel.SetActive(true);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
