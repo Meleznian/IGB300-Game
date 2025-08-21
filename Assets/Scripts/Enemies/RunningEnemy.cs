346c7d439162c1a8a6aeda18231f2c851cca361d
Meleznian <jwinmeleznia@gmail.com>
2025-08-21 18:25:36 +1000

    Enemy Does Damage

--- Assets/Scripts/Enemies/RunningEnemy.cs
+++ Assets/Scripts/Enemies/RunningEnemy.cs
@@ -12,4 +12,16 @@ public class RunningEnemy : EnemyBase
         if (transform.position.x < GameManager.instance.Player.transform.position.x) _moveDirection = Vector3.right;
         else _moveDirection = Vector3.left;
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<PlayerHealth>();
+        if (player != null)
+        {
+            print("Triggered: " + other.gameObject.name);
+            player.TakeDamage(defaultDamage);
+            Vector2 direction = ((-_moveDirection + Vector3.up)*actingMoveSpeed)*10;
+            rb.AddForce(direction, ForceMode2D.Impulse);
+        }
+    }
 }
