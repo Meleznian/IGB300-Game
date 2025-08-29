0b09c1d3823fce7c6b7c8bb6179f0d8413cc14c9
Meleznian <jwinmeleznia@gmail.com>
2025-08-29 17:18:56 +1000

    Added Opening Dialogue from Steam King

---
+++ Assets/Scripts/UI-Menus/Cutscene.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using EasyTextEffects;
+
+public class Cutscene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] string[] kingDialogue;
+    [SerializeField] TMP_Text text;
+    Animator anim;
+    [SerializeField] TextEffect effect;
+    void Start()
+    {
+        effect = text.gameObject.GetComponent<TextEffect>();
+        anim = GetComponent<Animator>();
+
+        Setup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Setup()
+    {
+        string newText = kingDialogue[UnityEngine.Random.Range(0, kingDialogue.Length)];
+        text.text = newText;
+
+        //if(UnityEngine.Random.Range(0,10000) == 1)
+        //{
+        //    text.text = "HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE";
+        //}
+
+        effect.Refresh();
+        effect.StartManualEffect("typewriter");
+        //anim.SetTrigger("Start");
+    }
+
+    public void Arrived()
+    {
+        print("arrived");
+    }
+
+    public void TextDone()
+    {
+        anim.SetTrigger("Leave");
+    }
+
+    public void Exited()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+}
