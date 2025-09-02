e06985f32da50ec7a19d851d953a56c485018041
Meleznian <jwinmeleznia@gmail.com>
2025-09-02 13:13:53 +1000

    Points Get Text now fade out

--- Assets/Scripts/VFX/RisingText.cs
+++ Assets/Scripts/VFX/RisingText.cs
@@ -1,27 +1,41 @@
 using UnityEngine;
+using TMPro;
 
 public class RisingText : MonoBehaviour
 {
     [SerializeField] float lifeTime;
     [SerializeField] float stopTime;
     [SerializeField] float riseSpeed;
+    [SerializeField] float fadeSpeed;
+    [SerializeField] TMP_Text text;
 
     float timer;
+
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
 
-        if(timer >= stopTime)
-        {
-            riseSpeed = 0;
-        }
+        //if (timer >= stopTime)
+        //{
+        //    riseSpeed = 0;
+        //}
         if (timer >= lifeTime)
         {
-            Destroy(gameObject);
+            Fade();
+            if (text.color.a <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
 
-       transform.position = new Vector2(transform.position.x, transform.position.y + (riseSpeed*Time.deltaTime));
+        transform.position = new Vector2(transform.position.x, transform.position.y + (riseSpeed * Time.deltaTime));
+    }
+
+
+    void Fade()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / fadeSpeed));
     }
 }
