d0d65500ea4378400b74d4446936f601b652f046
Meleznian <jwinmeleznia@gmail.com>
2025-09-18 22:40:37 +1000

    Removed Broken Namespace

--- Assets/Scripts/Mechanics/UpgradeManager.cs
+++ Assets/Scripts/Mechanics/UpgradeManager.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.Timeline.Actions;
+
 
 public class UpgradeManager : MonoBehaviour
 {
