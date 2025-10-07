8eb7ae7a257e68c05d25733da90d4b938e156f3d
Meleznian <jwinmeleznia@gmail.com>
2025-10-07 13:20:51 +1000

    Fixed Some bugs

--- Assets/Scripts/Level/TutorialExit.cs
+++ Assets/Scripts/Level/TutorialExit.cs
@@ -5,6 +5,11 @@ public class TutorialExit : MonoBehaviour
     [SerializeField] GameObject killWall;
     [SerializeField] PlayerMovement player;
 
+    private void Start()
+    {
+        player = GameManager.instance.Player.GetComponent<PlayerMovement>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         killWall.SetActive(true);
