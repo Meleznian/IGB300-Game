e85af39d106a4407d3a6c0ef5cefc1d5840bc0dd
Meleznian <jwinmeleznia@gmail.com>
2025-10-14 14:11:19 +1000

    Fixed the level chunks not despawning

--- Assets/Scripts/Level/ChunkController.cs
+++ Assets/Scripts/Level/ChunkController.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class ChunkController : MonoBehaviour
 {
     [SerializeField] private int _poolIndex;
-    [SerializeField] private float _despawnX = -30f;
+    [SerializeField] private float _despawnX = 30f;
+    [SerializeField] Transform player;
+    [SerializeField] float dist;
     //[SerializeField] private GameObject _killWall;
     //[SerializeField] private GameObject _thisChunk;
 
+    private void Start()
+    {
+        player = GameManager.instance.Player.transform;
+    }
+
     private void Update()
     {
-        if (transform.position.x < _despawnX)
+        dist = Vector2.Distance(transform.position, player.position);
+        print(dist );
+
+        if (dist > _despawnX && transform.position.x < player.transform.position.x)
         {
             ChunkPoolManager.Instance.ReturnChunkToPool(gameObject, _poolIndex);
         }
+
     }
 }
