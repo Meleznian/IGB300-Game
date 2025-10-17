b1aeba8f6a66df34fb4706ab7a3c40366a6662fa
RexLegendes <166970739+RexLegendes@users.noreply.github.com>
2025-10-17 11:15:19 +1000

    updated keys selection in main canvas to credit

--- Assets/Scripts/UI-Menus/MousecursorBlocker.cs
+++ Assets/Scripts/UI-Menus/MousecursorBlocker.cs
@@ -4,12 +4,18 @@ using UnityEngine.EventSystems;
 public class MousecursorBlocker : MonoBehaviour
 {
     public GameObject defaultButton;
+    void Start()
+    {
 
+    }
+
+    // The system will force selection of the default button to restore navigation
     void Update()
     {
         if (EventSystem.current.currentSelectedGameObject == null)
         {
             EventSystem.current.SetSelectedGameObject(defaultButton);
         }
+
     }
 }
