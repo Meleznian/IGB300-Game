3fc002275fd96cae91331c516880a1c062f90c1b
Yuma0707 <hillzenn@gmail.com>
2025-10-17 18:13:07 +1000

    Fixed Nuke's bugs

--- Assets/Scripts/PickupScript.cs
+++ Assets/Scripts/PickupScript.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -25,7 +26,6 @@ public class PickupScript : MonoBehaviour, ICollectable
     {
         player = GameObject.Find("Player").transform;
         StartCoroutine(CollectDelay());
-        //parent = transform.parent;
         rb = GetComponent<Rigidbody2D>();
         c = GetComponent<Collider2D>();
 
@@ -34,9 +34,9 @@ public class PickupScript : MonoBehaviour, ICollectable
             rb.gravityScale = 0.0f;
         }
     }
+
     void Update()
     {
-
         float distance = Vector2.Distance(transform.position, player.position);
         inRange = distance <= MagnetRange;
 
@@ -47,7 +47,6 @@ public class PickupScript : MonoBehaviour, ICollectable
 
         if (canBeCollected)
         {
-
             if (tagged)
             {
                 if (!floating)
@@ -73,31 +72,54 @@ public class PickupScript : MonoBehaviour, ICollectable
         switch (pickupType)
         {
             case 1:
-                //Heal
+                // Heal
                 PlayerHealth healScript = GameManager.instance.Player.GetComponent<PlayerHealth>();
                 healScript.Heal((int)(healScript.maxHealth * 0.4));
                 break;
+
             case 2:
-                //Nuke
+                // Nuke
                 Instantiate(particleEffect, GameManager.instance.Player.transform.position, Quaternion.identity);
-                for (int i = 0; i < EnemyManager.instance.livingEnemiesList.Count; i++)
+
+                // Create a copy to ensure safety even if the livingEnemiesList is modified
+                var enemies = new List<GameObject>(EnemyManager.instance.livingEnemiesList);
+
+                foreach (var enemy in enemies)
                 {
-                    EnemyManager.instance.livingEnemiesList[0].GetComponent<EnemyBase>().Die(true);
+                    if (enemy == null) continue;
+
+                    var root = enemy.GetComponentInParent<Transform>()?.gameObject ?? enemy;
+                    var baseScript = root.GetComponent<EnemyBase>();
+                    if (baseScript != null)
+                    {
+                        baseScript.Die(true);
+                        continue;
+                    }
+
+                    var dmg = root.GetComponentInParent<IDamageable>();
+                    if (dmg != null)
+                    {
+                        dmg.TakeDamage(999999);
+                    }
                 }
                 break;
+
             case 3:
-                //Slow Kill Wall
+                // Slow Kill Wall
                 GameManager.instance.killWall.gameObject.GetComponent<KillWall>().SlowKillWallPickup();
                 break;
+
             case 4:
-                //Money
+                // Money
                 Instantiate(particleEffect, GameManager.instance.Player.transform.position, Quaternion.identity);
                 GameManager.instance.BoltCount((float)(GameManager.instance.gameObject.GetComponent<UpgradeManager>().cashGoal * 0.3));
                 break;
+
             case 5:
-                //SuperCharge
+                // SuperCharge
                 GameManager.instance.Player.GetComponent<PlayerMeleeAttack>().RunSuperCharge();
                 break;
+
             default:
                 Debug.Log("Not Implemented Exception");
                 break;
@@ -127,7 +149,7 @@ public class PickupScript : MonoBehaviour, ICollectable
 
     public void Move()
     {
-        transform.position += _moveDirection * (speed*Time.deltaTime);
+        transform.position += _moveDirection * (speed * Time.deltaTime);
         transform.position = new Vector3(transform.position.x, A * Mathf.Sin(transform.position.x - B) + C, transform.position.z);
     }
 }
